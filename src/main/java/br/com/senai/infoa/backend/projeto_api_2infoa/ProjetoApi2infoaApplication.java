0afacf5d650fd1816db7388be3041c107cafbf00
gabrielfcardoso9 <gabriel.f.cardoso9@aluno.senai.br>
2026-09-18 11:35:16 -0300

    primeiro projeto

---
+++ src/main/java/br/com/senai/infoa/backend/projeto_api_2infoa/ProjetoApi2infoaApplication.java
@@ -0,0 +1,13 @@
+package br.com.senai.infoa.backend.projeto_api_2infoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoApi2infoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoApi2infoaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/senai/infoa/backend/projeto_api_2infoa/ProjetoApi2infoaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.senai.infoa.backend.projeto_api_2infoa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoApi2infoaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
